a8c0bdbbc30d316a6455a1f2f50de297d016c68a
Paulo Henrique <phenriq694@gmail.com>
2019-10-16 20:13:06 -0300

    BACKEND: Cadastrando estudantes usando o controller 'StudentController' quando a rota '/students' é acessada através do método 'POST'

--- gympoint/backend/src/routes.js
+++ gympoint/backend/src/routes.js
@@ -1,20 +1,9 @@
 import { Router } from 'express';
 
-import Student from './app/models/Student';
+import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const student = await Student.create({
-    name: 'Paulo Henrique',
-    email: 'phenriq@gmail.com',
-    password_hash: '1234145',
-    age: 24,
-    weight: 60.59,
-    height: 1.7,
-  });
-
-  res.send(student);
-});
+routes.post('/students', StudentController.store);
 
 export default routes;
