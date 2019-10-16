6da4a68e86e392de17c1a7041ecd84fd55a93944
Paulo Henrique <phenriq694@gmail.com>
2019-10-16 17:25:56 -0300

    BACKEND: Criação do loader de models

--- gympoint/backend/src/routes.js
+++ gympoint/backend/src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
+import Student from './app/models/Student';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.send('Hello Word');
+routes.get('/', async (req, res) => {
+  const student = await Student.create({
+    name: 'Paulo Henrique',
+    email: 'phenriq@gmail.com',
+    password_hash: '1234145',
+    age: 24,
+    weight: 60.59,
+    height: 1.7,
+  });
+
+  res.send(student);
 });
 
 export default routes;
