78e0794a1d5b49f7da29e4e72e77f7ead66398e5
Paulo Henrique <phenriq694@gmail.com>
2019-10-17 14:55:33 -0300

    BACKEND: Corrigindo informação passada nas  mensagens de erros

--- gympoint/backend/src/app/controllers/StudentController.js
+++ gympoint/backend/src/app/controllers/StudentController.js
@@ -22,7 +22,7 @@ class StudentController {
     }
 
     if (studentExists) {
-      return res.status(400).json({ error: 'User already exists' });
+      return res.status(400).json({ error: 'Student already exists' });
     }
 
     const { id, name, email, age, weight, height } = await Student.create(
@@ -61,7 +61,7 @@ class StudentController {
         const studentExists = await Student.findOne({ where: { email } });
 
         if (studentExists) {
-          return res.status(400).json({ error: 'User already exists' });
+          return res.status(400).json({ error: 'Student already exists' });
         }
       }
     }
